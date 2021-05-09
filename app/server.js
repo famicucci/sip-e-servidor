a8e447ab2fedcaea34a77ffd79c01a6d84a913a9
Francisco Micucci <famicucci@gmail.com>
2021-05-08 21:04:09 -0300

    Arranca el servidor

---
+++ app/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// rutas
+app.use('/', (req, res) => {
+	res.json({ hola: 'Mundo' });
+});
+
+// arrancar el servidor
+app.listen(PORT, () => {
+	console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
