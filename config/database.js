791488f5bd979a6044182000dfdaa3f0bacf8af0
Francisco Micucci <famicucci@gmail.com>
2021-05-09 13:49:26 -0300

    PORT=3306 y variables de Entorno

--- config/database.js
+++ config/database.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
+console.log(process.env.DB_DATABASE);
+
 module.exports = {
-	username: 'root',
-	password: '',
-	database: 'sip-e2',
-	host: 'localhost',
+	username: process.env.DB_USERNAME || 'root',
+	password: process.env.DB_PASSWORD || '',
+	database: process.env.DB_DATABASE || 'sip-e2',
+	host: process.env.DB_HOST || 'localhost',
 	dialect: 'mysql',
 };
