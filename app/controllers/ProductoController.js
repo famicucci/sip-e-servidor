50ccf4816cc2b417870ed89a0ca75c090be520c5
Francisco Micucci <famicucci@gmail.com>
2021-05-10 11:49:21 -0300

    Try catch en controlador productos

--- app/controllers/ProductoController.js
+++ app/controllers/ProductoController.js
@@ -1,14 +1,22 @@
 const { Producto } = require('../database/db');
 
 exports.traerProductos = async (req, res) => {
-	const productos = await Producto.findAll();
-	res.json(productos);
+	try {
+		const productos = await Producto.findAll();
+		res.json(productos);
+	} catch (error) {
+		res.json(error);
+	}
 };
 
 exports.crearProducto = async (req, res) => {
-	const producto = await Producto.create({
-		codigo: req.body.codigo,
-		descripcion: req.body.descripcion,
-	});
-	res.json(producto);
+	try {
+		const producto = await Producto.create({
+			codigo: req.body.codigo,
+			descripcion: req.body.descripcion,
+		});
+		res.json(producto);
+	} catch (error) {
+		res.json(error);
+	}
 };
