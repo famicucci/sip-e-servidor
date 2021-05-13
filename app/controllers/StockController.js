e6d856db121ebda08cee297b4a006e69494c89c7
Francisco Micucci <famicucci@gmail.com>
2021-05-13 16:21:59 -0300

    modificarStock tabla stocks

--- app/controllers/StockController.js
+++ app/controllers/StockController.js
@@ -1,36 +1,21 @@
 const { Stock } = require('../models/index');
 
-// crear
-// exports.crearStock = async (req, res) => {
-// 	try {
-// 		const stock = await Stock.create({
-// 			ProductoCodigo: req.body.ProductoCodigo,
-// 			cantidad: req.body.cantidad,
-// 			PtoStockId: req.body.PtoStockId,
-// 		});
-// 		res.json(stock);
-// 	} catch (error) {
-// 		console.log(req.body);
-// 		res.json(error);
-// 	}
-// };
-
 // modificar
 exports.modificarStock = async (req, res) => {
-	try {
-		const stock = await Stock.update(
-			{
-				cantidad: req.body.cantidad,
-			},
-			{
-				where: {
-					ProductoCodigo: req.params.ProductoCodigo,
-					PtoStockId: req.params.PtoStockId,
-				},
-			}
-		);
-		res.json({ success: 'El producto ha sido modificado' });
-	} catch (error) {
-		res.json(error);
-	}
+	const target = {
+		ProductoCodigo: req.body.ProductoCodigo,
+		PtoStockId: req.body.PtoStockId,
+	};
+
+	const modificaStock = await Stock.increment('cantidad', {
+		by: req.body.cantidad,
+		where: target,
+	});
+
+	// insert en tabla movimientos
 };
+
+// const comprueba = await Stock.findOne({
+// 	where: target,
+// });
+// res.json(comprueba);
--- app/routes/api/stockproductos.js
+++ app/routes/api/stockproductos.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const StockController = require('../../controllers/StockController');
 
-// router.post('/', StockController.crearStock);
-router.put('/:ProductoCodigo/:PtoStockId', StockController.modificarStock);
+router.put('/', StockController.modificarStock);
 
 module.exports = router;
