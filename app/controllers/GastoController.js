eb85332f2c208e54e10ff6bc49e9c840e5cb747a
Francisco Micucci <famicucci@gmail.com>
2021-06-23 16:59:25 -0300

    Trae, modifica y elimina gastos

--- app/controllers/GastoController.js
+++ app/controllers/GastoController.js
@@ -1,7 +1,11 @@
-const { Gasto, GastoCategoria, GastoSubcategoria } = require('../models/index');
+const {
+	Gasto,
+	GastoCategoria,
+	GastoSubcategoria,
+	Usuario,
+} = require('../models/index');
 
 exports.crearGasto = async (req, res) => {
-	console.log(req.body);
 	try {
 		const gasto = await Gasto.create({
 			descripcion: req.body.descripcion,
@@ -12,6 +16,71 @@ exports.crearGasto = async (req, res) => {
 			GastoSubcategoriaId: req.body.GastoSubcategoriaId,
 		});
 		res.json(gasto);
+	} catch (error) {
+		res.json(error);
+	}
+};
+
+exports.modificarGasto = async (req, res) => {
+	console.log(req.body);
+	try {
+		const gasto = await Gasto.update(
+			{
+				descripcion: req.body.descripcion,
+				estado: req.body.estado,
+				importe: req.body.importe,
+				UsuarioId: req.usuarioId,
+				GastoCategoriaId: req.body.GastoCategoriaId,
+				GastoSubcategoriaId: req.body.GastoSubcategoriaId,
+			},
+			{
+				where: { id: req.params.Id },
+			}
+		);
+
+		// verifica si el update fue exitoso
+		if (gasto[0]) {
+			res
+				.status(200)
+				.send({ msg: 'El gasto ha sido modificado!', severity: 'success' });
+		} else {
+			res
+				.status(400)
+				.send({ error: 'No se produjo ningún cambio en la base de datos' });
+		}
+	} catch (error) {
+		res.json(error);
+	}
+};
+
+exports.traerGastos = async (req, res) => {
+	try {
+		const gastos = await Gasto.findAll({
+			attributes: [
+				'id',
+				'descripcion',
+				'estado',
+				'importe',
+				'createdAt',
+				'updatedAt',
+				'UsuarioId',
+			],
+			include: [
+				{
+					model: GastoCategoria,
+					attributes: ['descripcion'],
+				},
+				{ model: GastoSubcategoria, attributes: ['descripcion'] },
+				{
+					model: Usuario,
+					attributes: ['usuario'],
+					where: { EmpresaId: req.usuarioEmpresaId },
+				},
+			],
+
+			raw: true,
+		});
+		res.json(gastos);
 	} catch (error) {
 		res.json(error);
 	}
@@ -29,6 +98,26 @@ exports.traerCatSubcat = async (req, res) => {
 			],
 		});
 		res.json(categorias);
+	} catch (error) {
+		res.json(error);
+	}
+};
+
+exports.eliminarGasto = async (req, res) => {
+	try {
+		const gasto = await Gasto.destroy({
+			where: { id: req.params.Id },
+		});
+
+		if (gasto) {
+			res
+				.status(200)
+				.send({ msg: 'El gasto ha sido eliminado!', severity: 'success' });
+		} else {
+			res
+				.status(400)
+				.send({ error: 'No se produjo ningún cambio en la base de datos' });
+		}
 	} catch (error) {
 		res.json(error);
 	}
--- app/routes/api/gastos.js
+++ app/routes/api/gastos.js
@@ -4,5 +4,8 @@ const GastoController = require('../../controllers/GastoController');
 
 router.get('/categorias', GastoController.traerCatSubcat);
 router.post('/', GastoController.crearGasto);
+router.get('/', GastoController.traerGastos);
+router.put('/:Id', GastoController.modificarGasto);
+router.delete('/:Id', GastoController.eliminarGasto);
 
 module.exports = router;
