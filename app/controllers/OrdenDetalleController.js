936c40e4b828f5b632d02799d5e63b3aa1bf168a
Francisco Micucci <famicucci@gmail.com>
2021-06-27 13:33:43 -0300

    Modificar detalle orden correcciones 1

--- app/controllers/OrdenDetalleController.js
+++ app/controllers/OrdenDetalleController.js
@@ -3,86 +3,6 @@ const { sequelize } = require('../models/index');
 
 // traer stock total y precios
 exports.modificarDetalleOrden = async (req, res) => {
-	// consultar el detalle
-	const detalle = await OrdenDetalle.findAll({
-		where: { OrdenId: req.params.OrdenId },
-	});
-
-	// borrar todo el detalle de la orden (productos)
-	await OrdenDetalle.destroy({
-		where: { OrdenId: req.params.OrdenId },
-	});
-
-	// // recuperar todo al stock
-	for (let i = 0; i < detalle.length; i++) {
-		const element = detalle[i];
-
-		await Stock.increment(
-			{ cantidad: element.cantidad },
-			{
-				// transaction: t,
-				where: {
-					ProductoCodigo: element.ProductoCodigo,
-					PtoStockId: element.PtoStockId,
-				},
-			}
-		);
-	}
-
-	res.json(detalle);
-	return;
-
-	// verificar que los productos tengan stock
-	// traer todos los stocks de productos
-	const stocks = await Stock.findAll({
-		attributes: { exclude: ['updatedAt'] },
-	});
-
-	let prodsSinStock = [];
-	let cantsFinales = [];
-	let detalleOrden = req.body;
-	for (let k = 0; k < detalleOrden.length; k++) {
-		const element = detalleOrden[k];
-		const cantProdCarr = element.cantidad;
-		const prodStock = stocks.find(
-			(x) =>
-				x.ProductoCodigo === element.ProductoCodigo &&
-				x.PtoStockId === element.PtoStockId
-		);
-
-		let cantProdStock;
-		if (prodStock) {
-			cantProdStock = prodStock.cantidad;
-		} else {
-			res.json({
-				msg: `El producto ${element.ProductoCodigo} o su punto de stock no se encuentran en la base de datos`,
-				severity: 'error',
-			});
-			return;
-		}
-
-		const cantfinal = cantProdStock - cantProdCarr;
-
-		// comparar cantidades con los productos del carrito para ver si estan disponibles
-		if (cantfinal < 0) {
-			prodsSinStock.push(element.ProductoCodigo);
-		} else {
-			cantsFinales.push({
-				ProductoCodigo: element.ProductoCodigo,
-				cantFinal: cantfinal,
-				PtoStockId: element.PtoStockId,
-			});
-		}
-	}
-
-	if (prodsSinStock.length > 0) {
-		// borrarlo del detalle de la orden
-		for (let j = 0; j < prodsSinStock.length; j++) {
-			const element = prodsSinStock[j];
-			detalleOrden = detalleOrden.filter((x) => x.ProductoCodigo !== element);
-		}
-	}
-
 	// crear los productos en el detalle
 	// crear los movimientos de stock correspondientes
 
@@ -90,6 +10,84 @@ exports.modificarDetalleOrden = async (req, res) => {
 	// variables que necesito que me devuelva el control de stock: cantsFinales, detalleOrden
 	const t = await sequelize.transaction();
 	try {
+		// consultar el detalle
+		const detalle = await OrdenDetalle.findAll({
+			where: { OrdenId: req.params.OrdenId },
+			transaction: t,
+		});
+
+		// borrar todo el detalle de la orden (productos)
+		await OrdenDetalle.destroy({
+			where: { OrdenId: req.params.OrdenId },
+			transaction: t,
+		});
+
+		for (let i = 0; i < detalle.length; i++) {
+			const element = detalle[i];
+
+			await Stock.increment(
+				{ cantidad: element.cantidad },
+				{
+					transaction: t,
+					where: {
+						ProductoCodigo: element.ProductoCodigo,
+						PtoStockId: element.PtoStockId,
+					},
+				}
+			);
+		}
+
+		// verificar que los productos tengan stock
+		// traer todos los stocks de productos
+		const stocks = await Stock.findAll({
+			attributes: { exclude: ['updatedAt'] },
+		});
+
+		let prodsSinStock = [];
+		let cantsFinales = [];
+		let detalleOrden = req.body;
+		for (let k = 0; k < detalleOrden.length; k++) {
+			const element = detalleOrden[k];
+			const cantProdCarr = element.cantidad;
+			const prodStock = stocks.find(
+				(x) =>
+					x.ProductoCodigo === element.ProductoCodigo &&
+					x.PtoStockId === element.PtoStockId
+			);
+
+			let cantProdStock;
+			if (prodStock) {
+				cantProdStock = prodStock.cantidad;
+			} else {
+				res.json({
+					msg: `El producto ${element.ProductoCodigo} o su punto de stock no se encuentran en la base de datos`,
+					severity: 'error',
+				});
+				return;
+			}
+
+			const cantfinal = cantProdStock - cantProdCarr;
+
+			// comparar cantidades con los productos del carrito para ver si estan disponibles
+			if (cantfinal < 0) {
+				prodsSinStock.push(element.ProductoCodigo);
+			} else {
+				cantsFinales.push({
+					ProductoCodigo: element.ProductoCodigo,
+					cantFinal: cantfinal,
+					PtoStockId: element.PtoStockId,
+				});
+			}
+		}
+
+		if (prodsSinStock.length > 0) {
+			// borrarlo del detalle de la orden
+			for (let j = 0; j < prodsSinStock.length; j++) {
+				const element = prodsSinStock[j];
+				detalleOrden = detalleOrden.filter((x) => x.ProductoCodigo !== element);
+			}
+		}
+
 		for (let i = 0; i < cantsFinales.length; i++) {
 			const element = cantsFinales[i];
 
@@ -104,6 +102,16 @@ exports.modificarDetalleOrden = async (req, res) => {
 				}
 			);
 		}
+
+		res.json(cantsFinales);
+		await t.commit();
+		return;
+
+		// debe crear el los registros con el OrdenId que viene en el params
+		detalleOrden = detalleOrden.map((x) => ({
+			...x,
+			OrdenId: req.params.OrdenId,
+		}));
 
 		const ordenDetalle = await OrdenDetalle.bulkCreate(detalleOrden, {
 			transaction: t,
@@ -129,7 +137,8 @@ exports.modificarDetalleOrden = async (req, res) => {
 				}
 			);
 		}
-
+		// El stock no está descontando bien!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		// devolver un mensaje si elimina un producto del carritooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 		await t.commit();
 		res.status(200).json(ordenDetalle);
 	} catch (error) {
