3ebea6121c3abb08562de73c36a2f8a2de6b462e
Francisco Micucci <famicucci@gmail.com>
2021-09-04 21:41:40 -0300

    Manages invoice status payment

--- app/controllers/PagoController.js
+++ app/controllers/PagoController.js
@@ -1,4 +1,4 @@
-const { Pago } = require('../models/index');
+const { Pago, Factura } = require('../models/index');
 
 exports.crearPago = async (req, res) => {
 	try {
@@ -9,8 +9,68 @@ exports.crearPago = async (req, res) => {
 			createdAt: req.body.createdAt,
 			UsuarioId: req.usuarioId,
 		});
+
+		let invoiceTotalAmount;
+		let paymentsTotalAmount = 0;
+
+		// get total amount of current invoice
+		const invoice = await Factura.findByPk(req.body.FacturaId);
+		if (invoice) invoiceTotalAmount = parseFloat(invoice.importeFinal);
+
+		// get all payments of current invoice
+		const payments = await Pago.findAll({
+			where: { FacturaId: req.body.FacturaId },
+		});
+
+		if (payments) {
+			payments.forEach((x) => {
+				paymentsTotalAmount += parseFloat(x.importe);
+			});
+		}
+
+		//  result
+		const result = invoiceTotalAmount - paymentsTotalAmount;
+
+		if (result === 0)
+			await Factura.update(
+				{
+					estadoPago: 'Pago',
+				},
+				{ where: { id: req.body.FacturaId } }
+			);
+
 		res.json(pagos);
 	} catch (error) {
-		res.json(error);
+		res.status(400).json({ msg: 'There was an error', severity: 'error' });
+	}
+};
+
+exports.cancelPayment = async (req, res) => {
+	try {
+		// select payment to be canceled
+		const payment = await Pago.findByPk(req.params.Id);
+
+		if (payment) {
+			const negativePayment = await Pago.create({
+				importe: -payment.importe,
+				FacturaId: payment.FacturaId,
+				MetodoPagoId: req.body.MetodoPagoId,
+				UsuarioId: req.usuarioId,
+			});
+
+			// update estadoPago in invoice with: 'Pendiente'
+			await Factura.update(
+				{
+					estadoPago: 'Pendiente',
+				},
+				{ where: { id: payment.FacturaId } }
+			);
+
+			res.status(200).json(negativePayment);
+		} else {
+			res.status(400).json({ msg: 'Payment doesn´t exist', severity: 'error' });
+		}
+	} catch (error) {
+		res.status(400).json({ msg: 'There was an error', severity: 'error' });
 	}
 };
--- app/routes/api/pagos.js
+++ app/routes/api/pagos.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const PagoController = require('../../controllers/PagoController');
 
 router.post('/', PagoController.crearPago);
+router.patch('/:Id', PagoController.cancelPayment);
 
 module.exports = router;
