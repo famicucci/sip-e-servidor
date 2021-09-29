f154f5d55beb94a5134c6a3ddf935b89d015cb5d
Francisco Micucci <famicucci@gmail.com>
2021-09-29 15:41:55 -0300

    Migrate ptoventa

--- app/database/migrations/20210622174448-create-pto-venta.js
+++ app/database/migrations/20210622174448-create-pto-venta.js
@@ -1,27 +1,30 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PtoVenta', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      descripcion: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PtoVenta');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('ptoventa', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			descripcion: {
+				type: Sequelize.STRING(30),
+				allowNull: false,
+			},
+			EmpresaId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'empresa', key: 'id' },
+			},
+			PtoStockId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'ptostock', key: 'id' },
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('ptoventa');
+	},
 };
