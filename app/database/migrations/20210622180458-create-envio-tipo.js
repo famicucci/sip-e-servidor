296de99ab744ecada23c852ef12c127ddc87a7b3
Francisco Micucci <famicucci@gmail.com>
2021-09-29 15:50:23 -0300

    Migrate tipoenvio

--- app/database/migrations/20210622180458-create-envio-tipo.js
+++ app/database/migrations/20210622180458-create-envio-tipo.js
@@ -1,27 +1,25 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('EnvioTipos', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      descripcion: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('EnvioTipos');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('tipoenvio', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			descripcion: {
+				type: Sequelize.STRING(30),
+				allowNull: false,
+			},
+			EmpresaId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'empresa', key: 'id' },
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('tipoenvio');
+	},
 };
