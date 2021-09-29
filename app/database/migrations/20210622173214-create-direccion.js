12fd2548b3221e51a410547533d3c30937fdf240
Francisco Micucci <famicucci@gmail.com>
2021-09-29 15:25:29 -0300

    Migrate direccion

--- app/database/migrations/20210622173214-create-direccion.js
+++ app/database/migrations/20210622173214-create-direccion.js
@@ -1,51 +1,55 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Direccions', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      calle: {
-        type: Sequelize.STRING
-      },
-      numero: {
-        type: Sequelize.INTEGER
-      },
-      piso: {
-        type: Sequelize.INTEGER
-      },
-      depto: {
-        type: Sequelize.INTEGER
-      },
-      barrio: {
-        type: Sequelize.STRING
-      },
-      ciudad: {
-        type: Sequelize.STRING
-      },
-      provincia: {
-        type: Sequelize.STRING
-      },
-      cp: {
-        type: Sequelize.INTEGER
-      },
-      referencia: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Direccions');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('direccion', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			calle: {
+				type: Sequelize.STRING(30),
+				allowNull: false,
+			},
+			numeroCalle: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			piso: {
+				type: Sequelize.STRING(3),
+			},
+			depto: {
+				type: Sequelize.STRING(10),
+			},
+			barrio: {
+				type: Sequelize.STRING(30),
+				allowNull: false,
+			},
+			ciudad: {
+				type: Sequelize.STRING(30),
+				allowNull: false,
+			},
+			provincia: {
+				type: Sequelize.STRING(30),
+			},
+			codPostal: {
+				type: Sequelize.STRING(11),
+				allowNull: false,
+			},
+			refDireccion: {
+				type: Sequelize.STRING(120),
+			},
+			ClienteId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'cliente', key: 'id' },
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('direccion');
+	},
 };
