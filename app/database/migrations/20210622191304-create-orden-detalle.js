88fa47d554fc1c33bcac1365a66c417cc29a80f3
Francisco Micucci <famicucci@gmail.com>
2021-09-29 16:05:54 -0300

    Migrate ordendetalle

--- app/database/migrations/20210622191304-create-orden-detalle.js
+++ app/database/migrations/20210622191304-create-orden-detalle.js
@@ -1,33 +1,50 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('OrdenDetalles', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      cantidad: {
-        type: Sequelize.INTEGER
-      },
-      pu: {
-        type: Sequelize.DECIMAL
-      },
-      origen: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('OrdenDetalles');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('ordendetalle', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			cantidad: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			pu: {
+				type: Sequelize.DECIMAL(10, 2),
+				allowNull: false,
+			},
+			origen: {
+				type: Sequelize.STRING(15),
+			},
+			BacklogProduccionId: {
+				type: Sequelize.INTEGER,
+			},
+			OrdenId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'orden', key: 'id' },
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			},
+			ProductoCodigo: {
+				type: Sequelize.STRING(15),
+				allowNull: false,
+				references: { model: 'producto', key: 'codigo' },
+				onDelete: 'NO ACTION',
+				onUpdate: 'NO ACTION',
+			},
+			PtoStockId: {
+				type: Sequelize.INTEGER,
+				references: { model: 'ptostock', key: 'id' },
+				onDelete: 'NO ACTION',
+				onUpdate: 'NO ACTION',
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('ordendetalle');
+	},
 };
