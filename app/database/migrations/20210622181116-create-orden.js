ce605b3b8cac1e0abc371b051882712b6637d52e
Francisco Micucci <famicucci@gmail.com>
2021-09-29 15:59:26 -0300

    Migrate orden

--- app/database/migrations/20210622181116-create-orden.js
+++ app/database/migrations/20210622181116-create-orden.js
@@ -1,39 +1,62 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Ordens', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      fecha: {
-        type: Sequelize.DATE
-      },
-      observaciones: {
-        type: Sequelize.STRING
-      },
-      direccionEnvio: {
-        type: Sequelize.STRING
-      },
-      tarifaEnvio: {
-        type: Sequelize.STRING
-      },
-      ordenEcommerce: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Ordens');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('orden', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			observaciones: {
+				type: Sequelize.STRING(120),
+			},
+			direccionEnvio: {
+				type: Sequelize.STRING(50),
+			},
+			tarifaEnvio: {
+				type: Sequelize.DECIMAL(10, 2),
+				allowNull: false,
+			},
+			ordenEcommerce: {
+				type: Sequelize.STRING(30),
+			},
+			createdAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			ClienteId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'cliente', key: 'id' },
+			},
+			PtoVentaId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'ptoventa', key: 'id' },
+			},
+			UsuarioId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'usuario', key: 'id' },
+			},
+			OrdenEstadoId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'ordenestado', key: 'id' },
+			},
+			TipoEnvioId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'tipoenvio', key: 'id' },
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('orden');
+	},
 };
