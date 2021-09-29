f5fb72448ecb4a96dc567f1430e4daa83ee3f534
Francisco Micucci <famicucci@gmail.com>
2021-09-29 16:24:00 -0300

    Migrate facturadetalle

--- app/database/migrations/20210622201011-create-factura-detalle.js
+++ app/database/migrations/20210622201011-create-factura-detalle.js
@@ -1,30 +1,38 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('FacturaDetalles', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      cantidad: {
-        type: Sequelize.INTEGER
-      },
-      pu: {
-        type: Sequelize.DECIMAL
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('FacturaDetalles');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('facturadetalle', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			cantidad: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			pu: {
+				type: Sequelize.DECIMAL(10, 2),
+				allowNull: false,
+			},
+			FacturaId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'factura', key: 'id' },
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			},
+			ProductoCodigo: {
+				type: Sequelize.STRING(15),
+				allowNull: false,
+				references: { model: 'producto', key: 'codigo' },
+				onDelete: 'NO ACTION',
+				onUpdate: 'NO ACTION',
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('facturadetalle');
+	},
 };
