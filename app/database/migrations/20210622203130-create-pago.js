5e426dda6bb6220c6f7ab409199889bbe33c4dff
Francisco Micucci <famicucci@gmail.com>
2021-09-29 22:39:11 -0300

    Migrate pago

--- app/database/migrations/20210622203130-create-pago.js
+++ app/database/migrations/20210622203130-create-pago.js
@@ -1,27 +1,50 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Pagos', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      importe: {
-        type: Sequelize.DECIMAL
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Pagos');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('pago', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			importe: {
+				type: Sequelize.DECIMAL(10, 2),
+				allowNull: false,
+			},
+			estado: {
+				type: Sequelize.STRING(1),
+				defaultValue: 'v',
+			},
+			tipo: {
+				type: Sequelize.STRING(3),
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			FacturaId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'factura', key: 'id' },
+			},
+			MetodoPagoId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'metodopago', key: 'id' },
+				onDelete: 'NO ACTION',
+				onUpdate: 'NO ACTION',
+			},
+			UsuarioId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: { model: 'usuario', key: 'id' },
+				onDelete: 'NO ACTION',
+				onUpdate: 'NO ACTION',
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('pago');
+	},
 };
