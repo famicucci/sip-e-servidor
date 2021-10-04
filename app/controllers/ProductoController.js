409f03edcb402798e6c23c84f826096c84b2c813
Francisco Micucci <famicucci@gmail.com>
2021-10-04 16:15:37 -0300

    Correct SELECT

--- app/controllers/ProductoController.js
+++ app/controllers/ProductoController.js
@@ -109,16 +109,16 @@ exports.traerProductosAMover = async (req, res) => {
 	try {
 		// const id = 2;
 		const productos = await sequelize.query(
-			`SELECT OrdenDetalle.ProductoCodigo, producto.Descripcion, OrdenDetalle.Cantidad, OrdenDetalle.PtoStockId AS ID_Pto_Stock_Producto , pto_stock_producto.Descripcion AS Pto_Stock_Producto_Descripcion, PtoVenta.PtoStockId AS ID_Pto_Stock_De_Pto_Venta, pto_stock_pto_venta.Descripcion AS Pto_Stock_Pto_Venta_Descripcion
-			FROM OrdenDetalle 
-			INNER JOIN producto ON OrdenDetalle.ProductoCodigo = producto.Codigo
-			INNER JOIN Orden ON OrdenDetalle.OrdenId = Orden.ID
+			`SELECT ordendetalle.ProductoCodigo, producto.Descripcion, ordendetalle.Cantidad, ordendetalle.PtoStockId AS ID_Pto_Stock_Producto , pto_stock_producto.Descripcion AS Pto_Stock_Producto_Descripcion, PtoVenta.PtoStockId AS ID_Pto_Stock_De_Pto_Venta, pto_stock_pto_venta.Descripcion AS Pto_Stock_Pto_Venta_Descripcion
+			FROM ordendetalle 
+			INNER JOIN producto ON ordendetalle.ProductoCodigo = producto.Codigo
+			INNER JOIN Orden ON ordendetalle.OrdenId = Orden.ID
 			INNER JOIN PtoVenta ON Orden.PtoVentaId = PtoVenta.ID
-			INNER JOIN PtoStock AS pto_stock_producto ON OrdenDetalle.PtoStockId = pto_stock_producto.ID
+			INNER JOIN PtoStock AS pto_stock_producto ON ordendetalle.PtoStockId = pto_stock_producto.ID
 			INNER JOIN PtoStock AS pto_stock_pto_venta ON PtoVenta.PtoStockId = pto_stock_pto_venta.ID
-			WHERE OrdenDetalle.Origen = 'Disponible'
+			WHERE ordendetalle.Origen = 'Disponible'
 			AND (Orden.OrdenEstadoId != '11')
-			AND OrdenDetalle.PtoStockId != PtoVenta.PtoStockId
+			AND ordendetalle.PtoStockId != PtoVenta.PtoStockId
 			AND producto.EmpresaId = '1'`,
 			{
 				type: QueryTypes.SELECT,
