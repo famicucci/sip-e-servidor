ca889291ab24a47cc284466de0d801358fff4538
Francisco Micucci <famicucci@gmail.com>
2021-10-15 18:28:22 -0300

    Put axios inside function

--- app/controllers/TiendaNubeController.js
+++ app/controllers/TiendaNubeController.js
@@ -1,18 +1,17 @@
 const axios = require('axios');
 
-const accessToken = '5c4d34b857d27539a980f5c659b28a45ce0459d1';
-const apiUrl = 'https://api.tiendanube.com';
-
-const tiendaNubeAxios = axios.create({
-	baseURL: apiUrl,
-	headers: {
-		Authentication: `bearer ${accessToken}`,
-		'User-Agent': 'API para Sip (famicucci@email.com)',
-	},
-});
-
 exports.traerProductos = async (req, res) => {
 	// res.status(200).json('traer productos');
+	const accessToken = '5c4d34b857d27539a980f5c659b28a45ce0459d1';
+	const apiUrl = 'https://api.tiendanube.com';
+
+	const tiendaNubeAxios = axios.create({
+		baseURL: apiUrl,
+		headers: {
+			Authentication: `bearer ${accessToken}`,
+			'User-Agent': 'API para Sip (famicucci@email.com)',
+		},
+	});
 	try {
 		const r = await tiendaNubeAxios.get('/v1/1894966/products');
 		res.status(200).json(r.data);
