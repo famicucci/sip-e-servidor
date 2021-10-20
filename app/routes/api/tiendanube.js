4a51f16aba31c5d832253995c921a5a65bcc2bb9
Francisco Micucci <famicucci@gmail.com>
2021-10-20 19:28:34 -0300

    Get orders and close order TN

--- app/routes/api/tiendanube.js
+++ app/routes/api/tiendanube.js
@@ -4,5 +4,7 @@ const TiendaNubeController = require('../../controllers/TiendaNubeController');
 
 router.get('/productos', TiendaNubeController.traerProductos);
 router.put('/stock/:ProductId/:VariantId', TiendaNubeController.modificarStock);
+router.get('/ordenes/', TiendaNubeController.traerOrdenes);
+router.post('/ordenes/:Id/close', TiendaNubeController.cancelOrder);
 
 module.exports = router;
